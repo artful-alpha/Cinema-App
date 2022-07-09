2527fd72984d934e04db8771938d491069855446
mccstrategymarketing.com.ua <mccstrategymarketing.com.ua>
2022-07-09 16:30:51 +0300

    Prepear develepment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      Hello
+      <ul>
+        <li>
+          <a href=''>1</a>
+        </li>
+        <li>
+          <a href=''>2</a>
+        </li>
+        <li>
+          <a href=''>3</a>
+        </li>
+      </ul>
     </div>
   );
 }
